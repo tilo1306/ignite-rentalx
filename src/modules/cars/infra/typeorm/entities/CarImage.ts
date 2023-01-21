f114166bfb08a94417e68b4f3b7f294a54cf8d3f
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-01-20 23:00:35 -0300

    feat: create entities CarImage

---
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -0,0 +1,17 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("Car_image")
+class CarImage {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  image_name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+export { CarImage };
