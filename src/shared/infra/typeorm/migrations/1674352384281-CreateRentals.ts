54525522d9a402849330319592ae286155e5fbf4
Douglas <doug1306@gmail.com>
2023-02-10 21:27:14 -0300

    feat:create migration CreateUsersToken

--- src/shared/infra/typeorm/migrations/1674352384281-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1674352384281-CreateRentals.ts
@@ -7,13 +7,44 @@ export class CreateRentals1674352384281 implements MigrationInterface {
         name: "rentals",
         columns: [
           { name: "id", type: "uuid", isPrimary: true },
-          { name: "car_id", type: "uuid" },
-          { name: "user_id", type: "uuid" },
-          { name: "start_date", type: "timestamp", default: "now(" },
-          { name: "end_date", type: "timestamp" },
-          { name: "expected_return_date", type: "timestamp" },
-          { name: "total", type: "numeric" },
-          { name: "created_at", type: "timestamp", default: "now()" },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "start_date",
+            type: "timestamp",
+            default: "now()",
+          },
+
+          {
+            name: "end_date",
+            type: "timestamp",
+            isNullable: true,
+          },
+          {
+            name: "expected_return_date",
+            type: "timestamp",
+          },
+          {
+            name: "total",
+            type: "numeric",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
         ],
         foreignKeys: [
           {
