20e3faf09a1413a4e8fba1b798df9b855c7b9d07
Douglas <doug1306@gmail.com>
2023-02-13 22:32:49 -0300

    refact: refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,4 @@
 import auth from "@config/auth";
-import { UserTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
@@ -16,8 +15,6 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
-  const userTokensRepository = new UserTokensRepository();
-
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
@@ -25,25 +22,14 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_Id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
-
-    const user = userTokensRepository.findByUserIdAndRefreshToken(
-      user_Id,
-      token
-    );
-
-    if (!user) {
-      throw new AppError("User does not exists!", 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
-      id: user_Id,
+      id: user_id,
     };
+
     next();
-  } catch (error) {
-    throw new AppError("Invalid Token", 401);
+  } catch {
+    throw new AppError("Invalid token!", 401);
   }
 }
