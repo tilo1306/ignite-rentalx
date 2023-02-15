3ad437662cf6b0ebfa49a7ad56fa47abd1b8b51d
Douglas <doug1306@gmail.com>
2023-02-15 20:42:02 -0300

    feat: add s3 project

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from "express";
+import { Request, Response } from "express";
 import { container } from "tsyringe";
 
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
@@ -7,6 +7,7 @@ class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
     const avatar_file = request.file.filename;
+
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
     await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
