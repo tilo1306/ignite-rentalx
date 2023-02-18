c561088de6174ce9cebc206dfacfbe8e8a521754
Douglas <doug1306@gmail.com>
2023-02-17 22:18:40 -0300

    refact: config connection database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,9 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultoptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultoptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       datebase:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
